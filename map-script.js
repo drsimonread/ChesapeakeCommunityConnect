2d5f1bdd98d84ffb0ed98cabb22e06e6446d509d
AlexStoyanov1 <awstoyanovroberts@smcm.edu>
2023-11-13 16:20:32 -0500

    Update map.js to fix create a post form
    Changed the script to allow the pop up to open

--- map-script.js
+++ map-script.js
@@ -1,66 +1,21 @@
-
-// In your existing script.js or a new JavaScript file:
 document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.getElementById("contact-form");
+    let map;
+    const addMarkerButton = document.getElementById("add-marker-button");
+    const addMarkerForm = document.getElementById("add-marker-form");
+    const userMarkerForm = document.getElementById("marker-form");
 
-    contactForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
-
-        const contactData = {
-            name,
-            email,
-            message,
-        };
-
-        // Send contactData to the server for saving to a JSON file
-        fetch("/submitContact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(contactData),
-        })
-            .then(response => response.json())
-            .then(data => {
-                // Handle the response from the server (e.g., show a success message)
-                console.log(data);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    });
-});
-
-
-// JavaScript for handling the map tab
-const mapTab = document.getElementById("map-tab");
-const addMarkerButton = document.getElementById("add-marker-button");
-const mapContainer = document.getElementById("map");
-const userMarkerForm = document.getElementById("marker-form");
-const addMarkerForm = document.getElementById("add-marker-form");
-
-let map; // Variable to hold the Google Map object
-const geocoder = new google.maps.Geocoder();
-const markers = [];
-
-// Initialize the map function
-function initMap() {
-    map = new google.maps.Map(mapContainer, {
-        center: { lat: 38.9, lng: -77.0 },
-        zoom: 8,
-    });
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 38.9, lng: -77.0 }, // Chesapeake Bay Area
+            zoom: 8,
+        });
+    }
 
     addMarkerButton.addEventListener("click", () => {
-        mapTab.style.display = "none";
         addMarkerForm.style.display = "block";
     });
 
-    // Event listener for the user marker form
-    userMarkerForm.addEventListener("submit", (event) => {
+    userMarkerForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const title = document.getElementById("title").value;
         const description = document.getElementById("description").value;
@@ -70,50 +25,31 @@ function initMap() {
         const zip = document.getElementById("zip").value;
         const fullAddress = `${address}, ${city}, ${state} ${zip}`;
 
-        geocoder.geocode({ address: fullAddress }, (results, status) => {
-            if (status === "OK" && results[0].geometry) {
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ address: fullAddress }, function (results, status) {
+            if (status === 'OK') {
                 const position = results[0].geometry.location;
-                const markerData = { title, description, position };
-                markers.push(markerData);
-
-                // Create a new marker on the map
                 const marker = new google.maps.Marker({
                     position,
                     map,
-                    title,
+                    title: title,
                 });
 
                 const infowindow = new google.maps.InfoWindow({
-                    content: `<h3>${title}</h3><p>${description}</p>`,
+                    content: `<h3>${title}</h3><p>${description}</p>`
                 });
 
-                marker.addListener("click", () => {
+                marker.addListener('click', function () {
                     infowindow.open(map, marker);
                 });
 
-                userMarkerForm.reset();
+                map.setCenter(position);
                 addMarkerForm.style.display = "none";
-                mapTab.style.display = "block";
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
             }
         });
     });
-}
 
-// Call the initMap function when the page loads
-google.maps.event.addDomListener(window, "load", initMap);
-
-// Function to create markers from widget data
-function createMarkersFromData(data) {
-    data.forEach((widget) => {
-        const position = new google.maps.LatLng(widget.position.lat, widget.position.lng);
-        const marker = new google.maps.Marker({
-            position,
-            map,
-            title: widget.title,
-        });
-
-        marker.addListener("click", () => {
-            infowindow.open(map, marker);
-        });
-    });
-}
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
