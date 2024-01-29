b24b7059a3d5d3882aa2bd3c71b055b0bf029e15
SamAlby <84740393+SamAlby@users.noreply.github.com>
2024-01-29 18:59:54 -0500

    fix maybe?

--- cccSite/boiler/apps 2.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BoilerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boiler'
--- cccSite/mapViewer/models 2.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class MapWidget(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    # Add other fields as needed
