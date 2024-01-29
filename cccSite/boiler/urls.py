3667b725247924adb0c1d8802e73347163e5376d
AlexStoyanov1 <awstoyanovroberts@smcm.edu>
2024-01-29 15:33:49 -0500

    Updating URLS/ base

--- cccSite/boiler/urls.py
+++ cccSite/boiler/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
    # path("", viewMap, name="viewMap"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    path('help/', views.help_page, name='help'),
+    path('help/', views.help, name='help'),
 ]
