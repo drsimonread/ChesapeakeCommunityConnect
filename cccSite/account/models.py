b93b112e4e6e72196f870a205a7c68e9dd74f1a1
DexterMue <144731991+DexterMue@users.noreply.github.com>
2024-01-30 16:40:33 -0500

    Commented account app
    Added comments to the templates, views, and models of the account django app

--- cccSite/account/models.py
+++ cccSite/account/models.py
@@ -3,27 +3,34 @@ from django.forms import ModelForm
 from .storage import OverwriteStorage
 
 
-
+#function used for saving images
 def user_directory_profile(instance, filename): 
-    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
+    # file will be uploaded to MEDIA_ROOT / user_<id> / <filename> 
     ext = filename.split('.')[-1]
     filename="profile."+ext
     return 'users/{0}/{1}'.format(instance.pk, filename) 
 
-# Create your models here.
+# user model. self explanitory
 class member(models.Model):
-    userID = models.CharField(max_length=255, unique=True)
+    userID = models.CharField(max_length=255, unique=True) #this will be deleted eventually, but right now this stores the google ID
     name = models.CharField(max_length=35)
     ranking = models.CharField(max_length=20, default="member")
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     email = models.EmailField()
-    origin = models.CharField(max_length=10, default="userpass")
     pic = models.ImageField(upload_to=user_directory_profile, storage = OverwriteStorage(), default='default/blankprof.png')
     about = models.TextField(blank=True, default="")
    #location = AddressField(blank=True, default={"raw" : ""}, null=True)
     def __str__(self):
         return self.name + "|" + self.userID
     #https://stackoverflow.com/questions/3715103/password-field-in-django-model/3715382#3715382 for making own password
+
+
+# not implemented yet
+# when user logs in with google, check the given google ID against this and then authenticate as user that is pointed to
+# same pattern can be used to implement other log in methods
+class gLogIn(models.Model):
+    googleID = models.CharField(max_length=255, unique=True)
+    pointTo = models.ForeignKey("member", on_delete = models.CASCADE)
 
 class manageForm(ModelForm):
     class Meta:
--- cccSite/account/views.py
+++ cccSite/account/views.py
@@ -9,13 +9,14 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 from PIL import Image
 
-# Create your views here.
+# if user not signed in, sends them to log in 
 def signin(request):
     if request.session.get('rank', 'anon')=='anon':
         return render(request, 'account/signin.html')
     else:
         return redirect("/account/")
 
+# if a user tries to sign out by URL, redirects to account. if there is a POST request to this url, flushes the session and sends them to confirmation
 def signout(request):
     if request.method == "GET":
         return redirect("/account/")
@@ -23,12 +24,13 @@ def signout(request):
         request.session.flush()
         return render(request, "account/signout.html")
 
-
+ 
+ # default account page. send to sign in if not signed in, otherwise displays user info
 def default(request):
     if request.session.get('rank','anon')=='anon':
         return redirect('/account/signin/')
     else:
-        userInz=models.member.objects.get(userID=request.session['user'])
+        userInz=models.member.objects.get(userID=request.session['user']) #get user from session
         return render(request, 'account/myaccount.html', {
             'name': userInz.name,
             'email' : userInz.email,
@@ -37,11 +39,12 @@ def default(request):
             'about' : userInz.about,
         })
 
+# lets members edit their info
 def manage(request):
     if request.session.get('rank','anon')=='anon':
         return redirect('/account/signin/')
     if request.method == "POST":
-        userInz=models.member.objects.get(userID=request.session['user'])
+        userInz=models.member.objects.get(userID=request.session['user']) 
         form = models.manageForm(request.POST, request.FILES, instance=userInz)
         if form.is_valid():
             form.save()
@@ -52,7 +55,7 @@ def manage(request):
         form = models.manageForm(instance=userInz)
     return render(request, "account/manage.html", {'form' : form})
 
-
+# a lot of this code is from google btw
 @csrf_exempt #the csrf is from google, not django, and is verified. can't get django's csrf to work tho due to origin of post
 def authG(request):
     if request.method == "GET":
@@ -60,7 +63,7 @@ def authG(request):
     elif request.method == "POST":
 
         csrf_tok_cookie = request.COOKIES.get('g_csrf_token')
-        
+        # check valid csrf token
         if not csrf_tok_cookie:
             return HttpResponse("Something went wrong, no csrf cookie")
         csrf_tok_body = request.POST.get('g_csrf_token')
@@ -68,16 +71,17 @@ def authG(request):
             return HttpResponse("Something went wrong, no csrf cookie from google")
         if csrf_tok_cookie != csrf_tok_body:
             return HttpResponse("Could not verify csrf")
-
+        #get token from google
         tok = request.POST.get("credential")  
         try:
+            # logs user in via their google ID, or makes an entry in member if they do not have an account yet.
             idinfo = id_token.verify_oauth2_token(tok, requests.Request(), "316865720473-94ccs1oka6ev4kmlv5ii261dirvjkja0.apps.googleusercontent.com")
             if not(models.member.objects.filter(userID=idinfo['sub']).exists()):
-                userInz = models.member.objects.create(userID=idinfo['sub'], name=idinfo['given_name'], email = idinfo['email'], origin='google')
+                userInz = models.member.objects.create(userID=idinfo['sub'], name=idinfo['given_name'], email = idinfo['email'])
             else:
                 userInz=models.member.objects.get(userID=idinfo['sub'])
             request.session['rank']=userInz.ranking
-            request.session['user']=userInz.userID
+            request.session['user']=userInz.userID # will need to change this just to the pk of username
             request.session['name']=userInz.name
         except ValueError:
             return HttpResponse("Something went wrong, invalid credentials from Google (somehow)")
