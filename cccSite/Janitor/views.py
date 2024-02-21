08748a55c7d5daaa29d913055d5099525532353d
DexterMue <144731991+DexterMue@users.noreply.github.com>
2024-02-21 11:10:41 -0500

    updated reports view in Janitor
    added logic for displaying reports by associating them with reported post and then showing posts with most reports first

--- cccSite/Janitor/views.py
+++ cccSite/Janitor/views.py
@@ -22,3 +22,32 @@ def memberList(request):
         memberStats.append({'user' : user, 'goodposts': visibleposts, 'reports':reports})
     
     return render(request, "Janitor/memberList.html", {'members' : memberStats,})
+
+def reportList(request):
+    pReports = PostReport.objects.all().order_by("post")
+    uReports = UserReport.objects.all().order_by("account")
+    currentPK = -1
+    pReportSorted = []
+    tempList = []
+    for report in pReports:
+        if report.post.pk != currentPK:
+            currentPK = report.post.pk
+            if len(tempList) != 0:
+                pReportSorted.append(tempList)
+            tempList = [report.post]
+        tempList.append(report)
+    currentPK = -1
+    uReportSorted = []
+    tempList = []
+    for report in uReports:
+        if report.account.pk != currentPK:
+            currentPK = report.account.pk
+            if len(tempList) != 0:
+                uReportSorted.append(tempList)
+            tempList = [report.account]
+        tempList.append(report)
+    pReportSorted.sort(key = len, reverse=True)
+    uReportSorted.sort(key = len, reverse=True)
+    print(pReportSorted)
+    print(uReportSorted)
+    return HttpResponse("hi")
