7b2652b87aa94a30dd7bf7d6ebf25f1db695728f
Sam Albanese <samuelpalbanese@gmail.com>
2024-02-26 15:35:09 -0500

    Update settings.py

--- cccSite/cccSite/settings.py
+++ cccSite/cccSite/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Add or modify the STATIC_URL setting to specify the URL for static files
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
