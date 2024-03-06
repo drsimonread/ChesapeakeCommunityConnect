ce355367a40418419aef12727e040c93ea05c80c
AlexStoyanov1 <awstoyanovroberts@smcm.edu>
2024-03-06 15:28:23 -0500

    Update urls.py

--- cccSite/mapViewer/urls.py
+++ cccSite/mapViewer/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "mapViewer"
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path("search/", views.post_list, name="post_list"),
     path("post/<int:want>", views.post_detail, name="post_detail"),
     # Add other URL patterns here
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
