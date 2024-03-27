57e23a572a7133dff6be852a23b3d6cd59460627
AlexStoyanov1 <awstoyanovroberts@smcm.edu>
2024-03-27 16:12:45 -0400

    Updated slideshow feature

--- cccSite/mapViewer/static/mapViewer/map-script2.js
+++ cccSite/mapViewer/static/mapViewer/map-script2.js
@@ -1,3 +1,23 @@
+// JavaScript for slideshow functionality
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
 async function initMap() {
 
     map = new google.maps.Map(document.getElementById("map"), {
--- cccSite/mapViewer/urls.py
+++ cccSite/mapViewer/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from mapViewer import views
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "mapViewer"
 
 urlpatterns = [
-    path('slideshow/<int:post_id>/', views.slideshow_popup, name='slideshow_popup'),
     path("", views.viewMap, name="default"),
     path("search/", views.post_list, name="post_list"),
-   
+    path("post/<int:want>", views.post_detail, name="post_detail"),
     # Add other URL patterns here
 ]
