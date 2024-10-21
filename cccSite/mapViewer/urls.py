29ba6c869ab20f154ec9c751586b88cccaf87122
roman-h-p-spiwak <roman.h.p.spiwak@gmail.com>
2024-10-21 16:25:30 -0400

    Readded the thing I just removed after adding it

--- cccSite/mapViewer/urls.py
+++ cccSite/mapViewer/urls.py
@@ -8,7 +8,7 @@ app_name = "mapViewer"
 urlpatterns = [
     path("", views.viewMap, name="default"),
     path("search/", views.forum_list, name="forum_list"),
-    path("forum/<int:want>", views.forum_detail, name="forum_detail"),
-    path("forum/<int:want>/post/<int:wants>", views.post_detail, name="post_detail"),
+    path("forum/<int:want>/", views.forum_detail, name="forum_detail"),
+    path("forum/<int:want>/post/<int:wants>/", views.post_detail, name="post_detail"),
     # Add other URL patterns here
 ]
