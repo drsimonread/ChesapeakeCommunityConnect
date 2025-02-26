6ccfbf33c786e1f0661b23ccd7def52d6ae8eb9d
Gus <CharlieGBlaH21@gmail.com>
2025-02-26 16:02:34 -0500

    Revert "Merge remote-tracking branch 'origin/development'"
    This reverts commit 83875a9f71746ff3b4e5433daea6a744f6090fa8, reversing
    changes made to d8e18e1eb08c032ebce9f985e5310ea0c730c509.

--- cccSite/account/models.py
+++ cccSite/account/models.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.db import models
-from django.contrib.auth.models import User
 from django.forms import ModelForm, TextInput, EmailInput, PasswordInput, Textarea
 from django.urls import reverse
 from .storage import OverwriteStorage
@@ -15,11 +14,8 @@ def user_directory_profile(instance, filename):
     return 'users/{0}/{1}'.format(instance.pk, filename) 
 
 # user model. self explanitory
-
-# MEMBER_DELETE
 class Member(models.Model):
-    # name = models.CharField(max_length=35)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=35, )
     ranking_options = { 
         (-1,"banned"),
         (1 , "member"),
@@ -28,14 +24,14 @@ class Member(models.Model):
         (99 , "admin"),
     }
     ranking = models.SmallIntegerField(default=1, choices=ranking_options)
-    # created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    # email = models.EmailField()
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    email = models.EmailField()
     pic = models.ImageField(upload_to=user_directory_profile, storage = OverwriteStorage(), default='default/blankprof.png')
     about = models.TextField(blank=True, default="")
 
    #location
     def __str__(self):
-        return self.user.username
+        return self.name
     @property
     def get_absolute_url(self):
         return reverse("account:account_view", args=[str(self.pk)])
@@ -49,38 +45,22 @@ class Member(models.Model):
 # same pattern can be used to implement other log in methods
 class GLogIn(models.Model):
     googleID = models.CharField(max_length=255, unique=True)
-    # MEMBER_DELETE
     referTo = models.ForeignKey(Member, on_delete = models.CASCADE)
     def __str__(self):
         return self.googleID + "|" + str(self.referTo)
 
 class ManageForm(ModelForm):
     class Meta:
-        # MEMBER_DELETE
         model = Member
-        fields = ["pic", "about"]
-        #! fields = ["pic", "name", "email", "about"]
+        fields = ["pic", "name", "email", "about"]
 
         widgets = {
             
-            #! 'name': forms.TextInput(attrs={'class':'nameField'}),
-            #! 'email':forms.EmailInput(attrs={'class':'emailField'}),
+            'name': forms.TextInput(attrs={'class':'nameField'}),
+            'email':forms.EmailInput(attrs={'class':'emailField'}),
             'about': forms.TextInput(attrs={'class':'aboutField'})
         }
         
-
-
-class UPLogIn(models.Model):
-    # MEMBER_DELETE
-    referTo = models.ForeignKey(Member, on_delete = models.CASCADE)
-    username = models.CharField(max_length=35, unique=True)
-    salt = models.CharField(max_length=22)
-    password = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.username + " | " + self.referTo.name
-
-
 class AccountCreation(models.Model):    
     email = models.EmailField()
     username = models.CharField(max_length=75)
