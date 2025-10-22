4b86ac805b45e7b705d5fa91f716d1228cfdcf8b
kellybyrne <atkellybyrne@gmail.com>
2025-10-22 19:46:19 +0000

    Add User Requests section to Django admin
    - Added created field to AccountCreation model to track request timestamps
    - Registered AccountCreation model with Django admin
    - Added AccountCreationAdmin with proper ordering (oldest to newest)
    - Added search, filtering, and fieldset organization for better UX
    - Created migration for the new created field
    
    This addresses the GitHub issue: Admin Page should include 'User Requests' section

--- cccSite/account/admin.py
+++ cccSite/account/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
 from . import models
+
 # Register your models here.
 admin.site.register(models.Member)
+
+@admin.register(models.AccountCreation)
+class AccountCreationAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'displayname', 'created')
+    list_filter = ('created',)
+    search_fields = ('email', 'username', 'displayname')
+    ordering = ('created',)  # Oldest to newest
+    readonly_fields = ('created',)
+
+    fieldsets = (
+        ('User Information', {
+            'fields': ('email', 'username', 'displayname')
+        }),
+        ('Account Details', {
+            'fields': ('password', 'confirmpassword')
+        }),
+        ('Request Information', {
+            'fields': ('created',),
+            'classes': ('collapse',)
+        }),
+    )
