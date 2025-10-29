1ea0f5d1cc2e14f09c9bb335225d20c864c92920
telesko15 <telesko@smcm.edu>
2025-10-29 20:24:13 +0000

    Revert "added the rank to what is returned"
    This reverts commit ea52a060b6c157dae26796900bc2a876bf177ca5.

--- cccSite/account/views.py
+++ cccSite/account/views.py
@@ -121,7 +121,6 @@ def account_list(request):
                 users=users.order_by("user__username")
     return render(request, 'account/account_list.html', {'users' : users,
                                                          'search' : search,
-                                                         'rank' : rank
                                                          })
 
 def my_forums(request):
