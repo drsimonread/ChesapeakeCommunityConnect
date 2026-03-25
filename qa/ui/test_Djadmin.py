afc27d42dada1df7fc5bd922c9d3b002b84ddba5
Ash42542703 <jslaufer@smcm.edu>
2026-03-25 20:10:17 +0000

    fixed Base_URLs

--- qa/ui/test_Djadmin.py
+++ qa/ui/test_Djadmin.py
@@ -5,5 +5,5 @@ BASE_URL = "http://127.0.0.1:8080/DJadmin/"
 
 def test_homepage():
     response = requests.get(BASE_URL + "/DJadmin/")
-    assert response.status_code < 500}"
+    assert response.status_code < 500
     assert "DJadmin" in response.text
--- qa/ui/test_homepage.py
+++ qa/ui/test_homepage.py
@@ -5,7 +5,6 @@ BASE_URL = "http://127.0.0.1:8080/"
 def test_homepage():
     response = requests.get(BASE_URL + "/homepage/")
     assert response.status_code == 200
-    expected_text = "Chesapeake Community Connect"
-    assert expected_text in response.text
+    assert "Chesapeake Community Connect" in response.text
 
     
