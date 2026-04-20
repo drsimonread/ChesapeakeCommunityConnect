a848119acffccdeef3dcba3b4a33109c4931efa2
zkralec <zkralec@icloud.com>
2026-04-20 16:12:57 +0000

    Repaired Django migration startup issues

--- cccSite/cccSite/settings.py
+++ cccSite/cccSite/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'cccSite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('CCCSITE_SQLITE_DB', BASE_DIR / 'db.sqlite3'),
     }
 }
 
